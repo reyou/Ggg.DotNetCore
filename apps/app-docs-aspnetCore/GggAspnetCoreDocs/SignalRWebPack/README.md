75f03d2f382d986391980e4081bf5045d5758329
teoman shipahi <aytekoz@yahoo.com>
2018-07-10 10:24:38 -0400

    SignalRWebPack
    SignalRWebPack

--- apps/app-docs-aspnetCore/GggAspnetCoreDocs/SignalRWebPack/README.md
+++ apps/app-docs-aspnetCore/GggAspnetCoreDocs/SignalRWebPack/README.md
@@ -7,4 +7,15 @@ https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr-typescript-webpac
 npm init -y
 npm install -D -E clean-webpack-plugin@0.1.19 css-loader@0.28.11 html-webpack-plugin@3.2.0 mini-css-extract-plugin@0.4.0 ts-loader@4.4.1 typescript@2.9.2 webpack@4.12.0 webpack-cli@3.0.6
 
+npm install @aspnet/signalr
 
+npm install webpack
+npm install webpack-cli
+npm install html-webpack-plugin
+npm install clean-webpack-plugin
+npm install mini-css-extract-plugin
+npm install ts-loader
+npm install typescript
+npm install css-loader
+
+$ npm run release => (webpack --mode=production)
--- apps/app-docs-aspnetCore/GggAspnetCoreDocs/SignalRWebPack/Startup.cs
+++ apps/app-docs-aspnetCore/GggAspnetCoreDocs/SignalRWebPack/Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SignalRWebPack.Hubs;
 
 namespace SignalRWebPack
 {
@@ -33,6 +29,8 @@ namespace SignalRWebPack
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSignalR();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,10 +47,18 @@ namespace SignalRWebPack
             }
 
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
             app.UseStaticFiles();
+
             app.UseCookiePolicy();
 
             app.UseMvc();
+
+            app.UseSignalR(options =>
+            {
+                options.MapHub<ChatHub>("/hub");
+            });
+
         }
     }
 }
