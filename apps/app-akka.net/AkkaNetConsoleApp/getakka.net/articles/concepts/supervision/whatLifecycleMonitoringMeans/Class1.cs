f297fcec00ffeea0636e4256e6fb4602782c7aad
aozdemir <aozdemir@webmd.net>
2018-11-02 00:10:50 -0400

    delayed-restarts-with-the-backoffsupervisor-pattern

---
+++ apps/app-akka.net/AkkaNetConsoleApp/getakka.net/articles/concepts/supervision/whatLifecycleMonitoringMeans/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Akka.Actor;
+using Akka.Pattern;
+using Akka.TestKit.TestActors;
+using Akka.TestKit.VsTest;
+using System;
+
+namespace AkkaNetConsoleApp.getakka.net.articles.concepts.supervision.whatLifecycleMonitoringMeans
+{
+    public class Class1 : TestKit
+    {
+        public Class1()
+        {
+            Props childProps = Props.Create<EchoActor>();
+            Props supervisor = BackoffSupervisor.Props(
+                Backoff.OnStop(
+                    childProps,
+                    "myEcho",
+                    TimeSpan.FromSeconds(3),
+                    TimeSpan.FromSeconds(30),
+                    randomFactor: 0.2));
+
+            Sys.ActorOf(supervisor, "echoSupervisor");
+
+        }
+    }
+}
