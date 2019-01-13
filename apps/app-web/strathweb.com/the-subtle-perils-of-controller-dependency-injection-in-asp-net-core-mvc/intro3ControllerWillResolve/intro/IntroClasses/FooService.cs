fe9b7eb02ae674775078edd3709e1a771e2e02f0
Teoman Shipahi <aytekoz@yahoo.com>
2019-01-13 14:06:18 -0500

    The Subtle Perils of Controller Dependency Injection in ASP.NET Core MVC | StrathWeb. A free flowing web tech monologue.

---
+++ apps/app-web/strathweb.com/the-subtle-perils-of-controller-dependency-injection-in-asp-net-core-mvc/intro3ControllerWillResolve/intro/IntroClasses/FooService.cs
@@ -0,0 +1,6 @@
+﻿namespace intro.IntroClasses
+{
+    public class FooService : IFooService
+    {
+    }
+}
---
+++ apps/app-web/strathweb.com/the-subtle-perils-of-controller-dependency-injection-in-asp-net-core-mvc/intro3ControllerWillResolve/intro/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using intro.Controllers;
+using intro.IntroClasses;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace intro
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            // Registers discovered controllers as services in the
+            // Microsoft.Extensions.DependencyInjection.IServiceCollection.
+            services.AddMvc().AddControllersAsServices();
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterType<FooService>().As<IFooService>();
+            builder.Populate(services);
+            IContainer container = builder.Build();
+            return container.Resolve<IServiceProvider>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
