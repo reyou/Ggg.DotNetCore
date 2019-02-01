a78ae89503f2dbe51c26b236bf6bd611a8386191
aozdemir <aozdemir@webmd.net>
2019-02-01 15:38:46 -0500

    excel import export

---
+++ apps/app-web/talkingdotnet.com/import-export-excel-asp-net-core-2-razor-pages/intro1/intro/Pages/ImportExport.cshtml.cs
@@ -0,0 +1,124 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace intro.Pages
+{
+    public class ImportExportModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public ActionResult OnPostImport()
+        {
+            IFormFile file = Request.Form.Files[0];
+            string folderName = "Upload";
+            string webRootPath = Startup.HostingEnvironment.WebRootPath;
+            string newPath = Path.Combine(webRootPath, folderName);
+            StringBuilder sb = new StringBuilder();
+            if (!Directory.Exists(newPath))
+            {
+                Directory.CreateDirectory(newPath);
+            }
+
+            if (file.Length > 0)
+            {
+                string sFileExtension = Path.GetExtension(file.FileName).ToLower();
+                string fullPath = Path.Combine(newPath, file.FileName);
+                using (FileStream stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                    stream.Position = 0;
+                    ISheet sheet;
+                    if (sFileExtension == ".xls")
+                    {
+                        HSSFWorkbook hssfwb = new HSSFWorkbook(stream); //This will read the Excel 97-2000 formats  
+                        sheet = hssfwb.GetSheetAt(0); //get first sheet from workbook  
+                    }
+                    else
+                    {
+                        XSSFWorkbook hssfwb = new XSSFWorkbook(stream); //This will read 2007 Excel format  
+                        sheet = hssfwb.GetSheetAt(0); //get first sheet from workbook   
+                    }
+                    IRow headerRow = sheet.GetRow(0); //Get Header Row
+                    int cellCount = headerRow.LastCellNum;
+                    sb.Append("<table class='table'><tr>");
+                    for (int j = 0; j < cellCount; j++)
+                    {
+                        ICell cell = headerRow.GetCell(j);
+                        if (cell == null || string.IsNullOrWhiteSpace(cell.ToString())) continue;
+                        sb.Append("<th>" + cell + "</th>");
+                    }
+                    sb.Append("</tr>");
+                    sb.AppendLine("<tr>");
+                    for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
+                    {
+                        IRow row = sheet.GetRow(i);
+                        if (row == null) continue;
+                        if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+                        for (int j = row.FirstCellNum; j < cellCount; j++)
+                        {
+                            if (row.GetCell(j) != null)
+                                sb.Append("<td>" + row.GetCell(j) + "</td>");
+                        }
+                        sb.AppendLine("</tr>");
+                    }
+                    sb.Append("</table>");
+                }
+            }
+            return Content(sb.ToString());
+        }
+
+        public async Task<IActionResult> OnPostExport()
+        {
+
+            string sWebRootFolder = Startup.HostingEnvironment.WebRootPath;
+            string sFileName = @"demo.xlsx";
+            var memory = new MemoryStream();
+            string combine = Path.Combine(sWebRootFolder, sFileName);
+            using (FileStream fs = new FileStream(combine, FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet excelSheet = workbook.CreateSheet("Demo");
+                IRow row = excelSheet.CreateRow(0);
+
+                row.CreateCell(0).SetCellValue("ID");
+                row.CreateCell(1).SetCellValue("Name");
+                row.CreateCell(2).SetCellValue("Age");
+
+                row = excelSheet.CreateRow(1);
+                row.CreateCell(0).SetCellValue(1);
+                row.CreateCell(1).SetCellValue("Kane Williamson");
+                row.CreateCell(2).SetCellValue(29);
+
+                row = excelSheet.CreateRow(2);
+                row.CreateCell(0).SetCellValue(2);
+                row.CreateCell(1).SetCellValue("Martin Guptil");
+                row.CreateCell(2).SetCellValue(33);
+
+                row = excelSheet.CreateRow(3);
+                row.CreateCell(0).SetCellValue(3);
+                row.CreateCell(1).SetCellValue("Colin Munro");
+                row.CreateCell(2).SetCellValue(23);
+
+                workbook.Write(fs);
+            }
+            using (var stream = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            FileStreamResult fileStreamResult = File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", sFileName);
+            return fileStreamResult;
+        }
+    }
+}
---
+++ apps/app-web/talkingdotnet.com/import-export-excel-asp-net-core-2-razor-pages/intro1/intro/README.md
@@ -0,0 +1,2 @@
+﻿Title:	ImportExport - intro
+Url:	http://localhost:59440/ImportExport
